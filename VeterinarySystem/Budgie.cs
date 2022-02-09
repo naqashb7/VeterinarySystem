e996a911419d343edfa55c7485b0cc4e9d29ae79
naqashb7 <79805483+naqashb7@users.noreply.github.com>
2022-02-09 16:10:13 +0000

    Adding extra classes

---
+++ VeterinarySystem/Budgie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeterinarySystem
+{
+    public class Budgie : Bird
+    {
+        public override void Eat(string food)
+        {
+            //implement later
+        }
+    }
+}
---
+++ VeterinarySystem/Reptile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeterinarySystem
+{
+    public abstract class Reptile : Animal
+    {
+        public override void Breathe()
+        {
+            //implement a breathe method
+        }
+
+        public override Reptile Reproduce(Animal otherParent)
+        {
+            return null;
+        }
+    }
+}
